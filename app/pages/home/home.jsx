583f16765c05dfafdbe2413225e44f2f930bfa4b
周力杰 <511863674@qq.com>
2017-12-29 12:36:27 +0800

    整体框架完成

---
+++ app/pages/home/home.jsx
@@ -0,0 +1,10 @@
+class home extends React.Component{
+
+    render () {
+        return [
+            <div className="list" key="{new Date().getTime()}">111</div>
+        ]
+    }
+}
+
+module.exports =  home
