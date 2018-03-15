d340b49ae2dece596f9e74365a2465089187eb06
周力杰 <511863674@qq.com>
2018-03-15 22:45:19 +0800

    add

---
+++ server.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var path = require('path');
+//var proxy = require('express-http-proxy');
+//const crypto = require('crypto');
+
+var app = express();
+
+app.get('/tonghuashun',function (req, res){
+	res.sendFile(path.join(__dirname, '../build/index.html'));
+});
+
+
+var server = app.listen(3030, function () {
+ 
+  var host = server.address().address
+  var port = server.address().port
+ 
+  console.log("访问地址为 http://%s:%s", host, port)
+ 
+});
+
